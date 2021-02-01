e344fd0a78ed710f9c7e53cdf0a041c4e42c8c38
jonathan <jonathancastro2018@yahoo.com>
2021-02-01 11:16:38 -0400

    avance con reponse

---
+++ src/Controller/UsersController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Controller;
+
+// agregar la entidad
+use App\Entity\Users;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UsersController extends AbstractController
+{
+    /**
+     * @Route("/users", name="users")
+     */
+    public function index(): Response
+    {
+        // return $this->render('users/index.html.twig', [
+        //     'controller_name' => 'UsersController',
+        // ]);
+
+        //  $entityManager = $this->getDoctrine()->getManager();
+
+        // $users = new Users();
+        // $users->setFirstName('jonathan');
+        // $users->setLastName('castro');
+        // $users->setEmail('admin@hotmail.com');
+
+        // // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        // $entityManager->persist($users);
+
+        // // actually executes the queries (i.e. the INSERT query)
+        // $entityManager->flush();
+
+        // return new Response('usuario registrado'.$users->getId());
+
+
+
+
+    	// $users = $this->getDoctrine()
+    	// ->getRepository(Users::class)
+    	// ->findAll();
+
+        // if (!$product) {
+        //     throw $this->createNotFoundException(
+        //         'No product found for id '.$id
+        //     );
+        // }
+
+
+    	$repository = $this->getDoctrine()->getRepository(Users::class);
+
+    	$users = $repository->findAll();
+
+    	return new Response('sd'.$users);
+
+    	// return $users;
+
+
+
+    }
+
+
+
+    // ejemplo para insertar
+    // public function index(): Response
+    // {
+    //     // return $this->render('users/index.html.twig', [
+    //     //     'controller_name' => 'UsersController',
+    //     // ]);
+
+    //      $entityManager = $this->getDoctrine()->getManager();
+
+    //     $users = new Users();
+    //     $users->setFirstName('jonathan');
+    //     $users->setLastName('castro');
+    //     $users->setEmail('admin@hotmail.com');
+
+    //     // tell Doctrine you want to (eventually) save the Product (no queries yet)
+    //     $entityManager->persist($users);
+
+    //     // actually executes the queries (i.e. the INSERT query)
+    //     $entityManager->flush();
+
+    //     return new Response('usuario registrado'.$users->getId());
+
+
+
+    // }
+}
