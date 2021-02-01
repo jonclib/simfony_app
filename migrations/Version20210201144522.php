d9525b92f6c6b5aff52dac10ed7502d8abe3613b
jonathan <jonathancastro2018@yahoo.com>
2021-02-01 10:51:11 -0400

    avances con migraciones y entidades

--- migrations/Version20210201144522.php
+++ migrations/Version20210201144522.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210201142455 extends AbstractMigration
+final class Version20210201144522 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,7 +20,7 @@ final class Version20210201142455 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, string VARCHAR(255) DEFAULT NULL, first_name VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, first_name VARCHAR(255) DEFAULT NULL, last_name VARCHAR(255) DEFAULT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -20,28 +20,21 @@ class Users
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
      */
-    private $string;
+    private $first_name;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
      */
-    private $first_name;
+    private $last_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getString(): ?string
-    {
-        return $this->string;
-    }
-
-    public function setString(?string $string): self
-    {
-        $this->string = $string;
-
-        return $this;
     }
 
     public function getFirstName(): ?string
@@ -52,6 +45,30 @@ class Users
     public function setFirstName(?string $first_name): self
     {
         $this->first_name = $first_name;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->last_name;
+    }
+
+    public function setLastName(?string $last_name): self
+    {
+        $this->last_name = $last_name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
 
         return $this;
     }
