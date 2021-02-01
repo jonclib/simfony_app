fd25f797086fe1b390a340026a3aea6a92aed10e
jonathan <jonathancastro2018@yahoo.com>
2021-02-01 10:30:26 -0400

    avance con migraciones y entidades

---
+++ src/Entity/Users.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UsersRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UsersRepository::class)
+ */
+class Users
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $string;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $first_name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getString(): ?string
+    {
+        return $this->string;
+    }
+
+    public function setString(?string $string): self
+    {
+        $this->string = $string;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->first_name;
+    }
+
+    public function setFirstName(?string $first_name): self
+    {
+        $this->first_name = $first_name;
+
+        return $this;
+    }
+}
